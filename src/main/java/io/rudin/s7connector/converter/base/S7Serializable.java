92f3c3ea0e4dd9723a3ec255615aadcd76f7800d
NatureFreshMilk <naturefreshmilk@github.com>
2016-05-18 09:00:56 +0200

    fix #1: license issue
    Use apache license in main code

--- src/main/java/io/rudin/s7connector/converter/base/S7Serializable.java
+++ src/main/java/io/rudin/s7connector/converter/base/S7Serializable.java
@@ -1,21 +1,17 @@
 /*
- Part of S7Connector, a connector for S7 PLC's
- 
- (C) Thomas Rudin (thomas@rudin-informatik.ch) 2012.
+Copyright 2016 Thomas Rudin
 
- S7Connector is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 3, or (at your option)
- any later version.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
- S7Connector is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+  http://www.apache.org/licenses/LICENSE-2.0
 
- You should have received a copy of the GNU General Public License
- along with Visual; see the file COPYING.  If not, write to
- the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 package io.rudin.s7connector.converter.base;
 
