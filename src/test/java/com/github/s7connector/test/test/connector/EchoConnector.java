af2e015be0d9fa0775b60a132ad0e6b8d27b5d45
NatureFreshMilk <naturefreshmilk@github.com>
2016-06-10 15:57:00 +0200

    rename dummy connector

--- src/test/java/com/github/s7connector/test/test/connector/EchoConnector.java
+++ src/test/java/com/github/s7connector/test/test/connector/EchoConnector.java
@@ -21,62 +21,58 @@ import com.github.s7connector.impl.S7Connector;
 import com.github.s7connector.impl.nodave.DaveArea;
 import com.github.s7connector.impl.utils.S7Type;
 
-public class DummyConnector implements S7Connector
-{
+/**
+ * Echo connector for testing
+ * 
+ * returns the same buffer in read() as given in write() regardless of the byte-range or area
+ * 
+ * @author Thomas Rudin
+ *
+ */
+public class EchoConnector implements S7Connector {
 
 	@Override
-	public byte[] read(DaveArea area, int areaNumber, int bytes, int offset)
-	{
+	public byte[] read(DaveArea area, int areaNumber, int bytes, int offset) {
 		return buffer;
 	}
-	
+
 	public byte[] buffer;
 
-
 	@Override
-	public byte[] readBlock(int blockNumber, int bytes, int offset)
-	{
+	public byte[] readBlock(int blockNumber, int bytes, int offset) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public <T> T readObject(Class<T> javaType, S7Type s7type, DaveArea area,
-			int areaNumber, int byteOffset, int bitOffset)
-	{
-		// TODO Auto-generated method stub
-		return null;
+	public <T> T readObject(Class<T> javaType, S7Type s7type, DaveArea area, int areaNumber, int byteOffset,
+			int bitOffset) {
+		
+		throw new IllegalArgumentException("not implemented");
 	}
 
 	@Override
-	public void write(DaveArea area, int areaNumber, int offset, byte[] buffer)
-	{
+	public void write(DaveArea area, int areaNumber, int offset, byte[] buffer) {
 		this.buffer = buffer;
-		
+
 		System.out.println("Size: " + buffer.length);
-		
-		for (int i=0; i<buffer.length; i++)
-			System.out.print( Integer.toHexString(buffer[i] & 0xFF) + ",");
-		
+
+		for (int i = 0; i < buffer.length; i++)
+			System.out.print(Integer.toHexString(buffer[i] & 0xFF) + ",");
+
 	}
 
 	@Override
-	public void writeBlock(int blockNumber, int offset, byte[] buffer)
-	{
+	public void writeBlock(int blockNumber, int offset, byte[] buffer) {
 		this.buffer = buffer;
-		
+
 		System.out.println("Size: " + buffer.length);
-		
-		for (int i=0; i<buffer.length; i++)
-			System.out.print( Integer.toHexString(buffer[i] & 0xFF) + ",");		
+
+		for (int i = 0; i < buffer.length; i++)
+			System.out.print(Integer.toHexString(buffer[i] & 0xFF) + ",");
 	}
 
 	@Override
-	public void close() throws IOException
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
+	public void close() throws IOException {}
 
 }
